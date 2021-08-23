f071318e15a3b00142c7aca5f17372ce4784d124
Diogo Cerqueira <diogosantoscerqueira@gmail.com>
2021-08-23 11:26:52 -0300

    Primeiro Commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @method - Proibir o acesso direto ao index.php && followlinks
+ */
+if($_SERVER['REQUEST_URI'] == $_SERVER['PHP_SELF']){
+    die(include('./layout/errors/404.html'));
+}
+
+// Importar Arquivos..
