54ff3fd8c80dd33d9ee2541dd33fdcf69840fb2a
Diogo Cerqueira <diogosantoscerqueira@gmail.com>
2021-08-23 14:36:40 -0300

    Composer e Classe Router importados e fixados

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @method - Proibir o acesso direto ao index.php && followlinks
  */
@@ -6,4 +7,23 @@ if($_SERVER['REQUEST_URI'] == $_SERVER['PHP_SELF']){
     die(include('./layout/errors/404.html'));
 }
 
-// Importar Arquivos..
+/**
+ * @method import composer
+ */
+require_once('./vendor/autoload.php');
+/**
+ * @package export
+ */
+use Router\Router;
+/**
+ * @global object Router
+ */
+$GLOBALS['Router'] = new Router();
+/**
+ * @method import router pages
+ */
+require_once('./router/pages.php');
+/**
+ * @param timezone
+ */
+date_default_timezone_set('America/Sao_Paulo');
