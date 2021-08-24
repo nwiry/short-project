bb7661e77e7700b191020b4107a21cee97e3ac4e
Diogo Cerqueira <diogosantoscerqueira@gmail.com>
2021-08-24 09:17:49 -0300

    Construtor fixado

--- system/classes/createfile.php
+++ system/classes/createfile.php
@@ -4,9 +4,39 @@
  * @method local
  */
 namespace Short\ShortProject\CreateFile;
+use Short\ShortProject\ChangeFile\ChangeFile;
+use PragmaRX\Random\Random;
 /**
  * @method class CreateFile
  */
-class CreateFile{
+class CreateFile extends ChangeFile{
+    /**
+     * @var object
+     */
+    private $random;
+    /**
+     * @param string
+     */
+    private $dirLinks;
+    /**
+     * @param string
+     */
+    private $extLinks;
+
+    /** ---- Construtor --- */
+    public function __construct(){
+        /**
+         * @return object
+         */
+        $this->random = new Random();
+        /**
+         * @return string
+         */
+        $this->dirLinks = parent::dirShorts;
+        /**
+         * @return string
+         */
+        $this->extLinks = parent::extShorts;
+    }
     
 }
