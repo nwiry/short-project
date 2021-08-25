8964ce0b483a5c400f8b8abf090018035588cb0e
Firmino Andre <andrezfirmino@gmail.com>
2021-08-25 08:56:41 -0300

    ajustes na pagina principal

--- layout/principal.php
+++ layout/principal.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
+
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,5 +13,34 @@
     <div class="logo">
         <img class="logo" src="assets/img/logo2.png">
     </div>
+
+    
+    <div class="container">
+  <div class="card">
+    <div class="imgBx">
+      <img src="https://cdn.discordapp.com/attachments/674731904230228055/879449919000289350/image-removebg-preview_20.png">
+    </div>
+    <div class="contentBx">
+      <h2>Por que encurtar links com a Guardaê</h2>
+      <div class="size">
+        <h3>Nosso tempo de resposta nos clicks são mais rápidos e precisos em relação a outros encurtadores</h3>
+      </div>
+      
+      <a href="#">Experimente</a>
+    </div>
+    
+  </div>
+
+</div>
+    <div class="textCenter">
+        <h1>Começe a usar agora mesmo</h1>
+    </div>
+
+<div class="inputCenter">
+<input class="input input-gradient" type="text" placeholder="Digite seu link"/>
+</div>
+
+
+
 </body>
 </html>
