1a9a832f6327248ee92964da471f9e5dfcf40a9f
Diogo Cerqueira <diogosantoscerqueira@gmail.com>
2021-08-25 16:24:56 -0300

    Funçao para validar URLs add

--- system/classes/validations.php
+++ system/classes/validations.php
@@ -11,4 +11,14 @@ class Validations{
      * @var array - Dominios banidos (Não podem ser encurtados aqui)
      */
     const BanUrls = [];
+
+    /**
+     * @return bool
+     */
+    private function validar_url($url){
+        if(filter_var($url, FILTER_VALIDATE_URL)){
+            return true;
+        }
+        return false;
+    }
 }
