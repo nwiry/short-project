df3161ad5cdd87080ed58f5f6b9f635ddb811fe0
Diogo Cerqueira <diogosantoscerqueira@gmail.com>
2021-09-04 19:55:13 -0300

    Objetos substituidos por array

--- system/apis/goto.php
+++ system/apis/goto.php
@@ -15,36 +15,30 @@ if($verificaLink->exist_result($endpoint)){
     /**
      * @var object
      */
-    $linkObjs = $verificaLink->objects_content($endpoint);
-    // Verifica se o short informado corresponde ao inserido no arquivo
-    if($endpoint == $linkObjs->short){
-        // Verifica se o link não possui proteção com senha
-        if(!$linkObjs->private){
-            // Contabilizar clique
-            try{
-                $changeFile->updateClick($endpoint, [
-                    "newValue" => $linkObjs->clicks
-                ]);
-                // Se contabilizou redirecione
-                header("location: " . $linkObjs->link);
-                // Encerre a execução
-                die;
-            }catch(Exception $e){
-                // Se nao contabilizou, redirecione, mas não pare o processo
-                header("location: " . $linkObjs->link);
-                // Encerre a execução
-                die;
-            }
-        }else{
-            // Verificar tipo de proteção e tratar dados
-            /**
-             * @method - Ate lançar a solução
-             */
-            die(header("location: /"));
+    $linkObjs = $verificaLink->array_content($endpoint);
+    // Verifica se o link não possui proteção com senha 
+    if(!$linkObjs[$endpoint]["private"]){
+        // Contabilizar clique 
+        try{
+            $changeFile->updateClick($endpoint, [
+                "newValue" => $linkObjs[$endpoint]["clicks"]
+            ]);
+            // Se contabilizou redirecione
+            header("location: " . $linkObjs[$endpoint]["link"]);
+            // Encerre a execução
+            die;
+        }catch(Exception $e){
+            // Se nao contabilizou, redirecione, mas não pare o processo
+            header("location: " . $linkObjs[$endpoint]["link"]);
+            // Encerre a execução
+            die;
         }
     }else{
-    // Link não corresponde ao conteudo na short
-    die(include_once(__DIR__ . '/../../layout/errors/404.html'));
+        // Verificar tipo de proteção e tratar dados
+         /**
+          * @method - Ate lançar a solução
+          */
+        die(header("location: /"));
     }
 }else{
     // Link não corresponde ao conteudo na short
